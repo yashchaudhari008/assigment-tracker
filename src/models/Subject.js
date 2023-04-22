c336f27b9d1bc57aed2b26a15cb8788231af8d32
Yash Chaudhari <yashchaudhari008@gmail.com>
2023-04-22 10:39:47 +0530

    API fixed!

--- src/models/Subject.js
+++ src/models/Subject.js
@@ -6,8 +6,8 @@ const SubjectSchema = new mongoose.Schema(
 		assignments: [
 			{
 				name: { type: String, required: true, trim: true },
-				completed: { type: Boolean},
-				sumbitted: { type: Boolean},
+				completed: { type: Boolean },
+				submitted: { type: Boolean },
 			},
 		],
 	},
@@ -18,47 +18,45 @@ const SubjectSchema = new mongoose.Schema(
 );
 
 SubjectSchema.methods = {
-    addAssignment: async function (name) {
-        const newAssignment = {
-            name: name,
-            completed: false,
-            sumbitted: false,
-        };
-        const exists = this.assignments.some((entry) =>
-            entry.name === name ? true : false
-        ); //Already exists
-        if (!exists) {
-            this.assignments = this.assignments.concat(newAssignment);
-        }
-    
-        await this.save();
-    },
-    deleteAssignment: async function (name) {
-        this.assignments = this.assignments.filter((entry) =>
-            entry.name !== name
-        );
-        await this.save();
-    },
-    markAsCompleteAssignment:  async function (name, completed) {
-        const exists = this.assignments.find((entry) =>
-            entry.name === name
-        );
-        if(exists){
-            exists.completed =completed;
-        }
-        await this.save();
-    },
-    markAsSumbittedAssignment:  async function (name, submitted) {
-        const exists = this.assignments.find((entry) =>
-            entry.name === name
-        );
-        if(exists){
-            exists.submitted =submitted;
-        }
-        await this.save();
-    }
+	addAssignment: async function (name) {
+		const exists = this.assignments.some((entry) =>
+			entry.name === name ? true : false
+		); //Already exists
+		if (!exists) {
+            const newAssignment = {
+                name: name,
+                completed: false,
+                submitted: false,
+            };
+			this.assignments = this.assignments.concat(newAssignment);
+		}
 
-}
+		await this.save();
+	},
+	deleteAssignment: async function (name) {
+		this.assignments = this.assignments.filter((entry) => entry.name !== name);
+		await this.save();
+	},
+	markAsCompleteAssignment: async function (name, completed) {
+		this.assignments = this.assignments.map((entry) => {
+			if (entry.name === name) {
+				entry.completed = completed;
+			}
+			return entry;
+		});
+		await this.save();
+	},
+	markAsSubmittedAssignment: async function (name, submitted) {
+		this.assignments = this.assignments.map((entry) => {
+			if (entry.name === name) {
+				entry.completed = true;
+				entry.submitted = submitted;
+			}
+			return entry;
+		});
+		await this.save();
+	},
+};
 
 export default mongoose.models.Subject ||
 	mongoose.model("Subject", SubjectSchema);
--- src/pages/api/subject/assignment.ts
+++ src/pages/api/subject/assignment.ts
@@ -23,21 +23,19 @@ export default async function handler(
 		await subjectFound.addAssignment(assignment);
 		return res.status(201).json({ success: true, data: subjectFound });
 	} else if (req.method === "PATCH") {
-		const { subject, assignment, completed, sumbitted } = req.body;
-        if(completed !== null){
-            const subjectFound = await Subject.findOne({ subject: subject });
-            await subjectFound.markAsCompleteAssignment(assignment, completed);
-            return res.status(201).json({ success: true, data: subjectFound });
-        } else if(sumbitted !== null) {
-			const subjectFound = await Subject.findOne({ subject: subject });
-            await subjectFound.markAsSumbittedAssignment(assignment, sumbitted);
-            return res.status(201).json({ success: true, data: subjectFound });
-		}
-    } else if (req.method === "DELETE") {
+		const { subject, assignment, completed, submitted } = req.body;
+
+		const subjectFound = await Subject.findOne({ subject: subject });
+		if (completed)
+			await subjectFound.markAsCompleteAssignment(assignment, completed);
+		if (submitted)
+			await subjectFound.markAsSubmittedAssignment(assignment, submitted);
+		return res.status(201).json({ success: true, data: subjectFound });
+	} else if (req.method === "DELETE") {
 		const { subject, assignment } = req.query;
 		const subjectFound = await Subject.findOne({ subject: subject });
 		if (subjectFound) {
-            await subjectFound.deleteAssignment(assignment);
+			await subjectFound.deleteAssignment(assignment);
 			return res.status(201).json({ success: true, data: subjectFound });
 		}
 		return res.status(400).json({ success: false });
